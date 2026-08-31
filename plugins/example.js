2d13aeef4245cd5707b2c1bcaac9cf000e271ac3
vanndx <vanndx26@gmail.com>
2026-08-31 18:40:14 +0700

    feat: add empty plugin template

---
+++ plugins/example.js
@@ -0,0 +1,13 @@
+/**
+ * Example plugin.
+ *
+ * This file is intentionally disabled and contains no bot feature.
+ * Copy it when you want to add your own command.
+ */
+
+export default {
+  name: 'example',
+  async execute({ conn, message, args }) {
+    // Your feature implementation goes here.
+  }
+}
